5a43071170de18db2d728ba92233716d6f4d04a7
Gautam-2604 <gaurisariag@gmail.com>
2025-02-06 01:25:28 +0530

    Quiz Module Completed and done

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+
+import { IoManager } from './managers/IoManager'
+
+
+
+const io = IoManager.getIo() 
+
+io.listen(3000)
+ 
+io.on('connection', (client)=>{
+    client.on('event', data=>{
+        console.log(data) ;
+        
+    })
+    client.on('disconnect',()=>{})
+})
